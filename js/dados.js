0477c99265c861d581247564bebb65c17931feb5
Gabriel Bueno <gabrielbuenojacino@gmail.com>
2024-09-14 13:51:58 -0300

    envio1

---
+++ js/dados.js
@@ -0,0 +1,85 @@
+const sobreMim = [
+    {
+        paragrafo: "Hello word, sejam bem vindos ao meu domínio, me chamo Gabriel, sou nascido e criado em uma pequena cidade no interior de Minas Gerais. Já estou no mercado de trabalho há 3 anos, tendo experiência principalmente na área administrativa em órgãos públicos, onde desenvolvi sólidas competências interpessoais e organizacionais, essenciais para a gestão eficiente de processos e pessoas. Recentemente, finalizei a minha graduação em Sistemas de Informação, o que despertou a minha paixão pela área de tecnologia, com foco no desenvolvimento web."
+        
+    },
+    {
+        paragrafo: "Desde então, tenho dedicado tempo ao estudo e aprimoramento das minhas habilidades em linguagens como HTML5, CSS3 e JavaScript. O meu objetivo atual é fazer a transição para a área de desenvolvimento web, onde poderei aplicar o meu conhecimento técnico e a experiência adquirida no setor administrativo, de forma a agregar valor em ambientes dinâmicos e tecnológicos."
+    },
+    {
+        paragrafo: "Estou motivado a contribuir para projetos que exijam inovação e criatividade, mantendo sempre o foco no desenvolvimento contínuo e na aplicação de soluções eficazes."
+    }
+]
+const formacaoCursos = [
+    {
+        nome: "Sistemas de informação",
+        ano: "2020 a 2023",
+        instituicao: "UNA",
+        sintese: "Desenvolvimento, implementação e gerenciamento de sistemas computacionais que respondem às necessidades das empresas, com foco em programação, bases de dados, redes e otimização de processos de negócios."
+    },
+    {
+        nome: "HTML e CSS: ambientes de desenvolvimento, estrutura de arquivos e tags",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Criação ambientes de desenvolvimento, organização de estrutura de arquivos e utilização tags para construir a estrutura e o design de páginas web."
+    },
+    {
+        nome: "HTML e CSS: cabeçalho, footer e variáveis CSS",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Criação de cabeçalhos e footers para estruturar páginas web, além de utilizar variáveis CSS para manter um estilo consistente e facilitar a manutenção do design."
+    },
+    {
+        nome: "HTML e CSS: Classes, posicionamento e Flexbox",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Utilização de classes para estilização personalizada, técnicas de posicionamento de elementos e o sistema Flexbox para criar layouts responsivos e flexíveis."
+    },
+    {
+        nome: "HTML e CSS: praticando HTML/CSS",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Criação de páginas web, aplicando conceitos de estrutura e estilização para desenvolver layouts completos e funcionais."
+    },
+    {
+        nome: "HTML e CSS: trabalhando com responsividade e publicação de projetos",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Implementação de design responsivo para adaptar páginas a diferentes dispositivos e publicação de projetos online, tornando-os acessíveis na web."
+    },
+    {
+        nome: "Java: aplicando a Orientação a Objetos",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Aplicação dos princípios da Programação Orientada a Objetos, criando classes, objetos, herança e polimorfismo para desenvolver aplicações modulares e escaláveis."
+    },
+    {
+        nome: "Java: consumindo API, gravando arquivos e lidando com erros",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Utilização de APIs, gravação e manipulação arquivos, além de implementação técnicas de tratamento de erros para garantir a robustez das aplicações."
+    },
+    {
+        nome: "Java: criando a sua primeira aplicação",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Construção de programas básicos levando em consideração a interação com o usuário. Compreenção dos fundamentos da linguagem e a lógica por trás da programação."
+    },
+    {
+        nome: "Java: trabalhando com listas e coleções de dados",
+        ano: "2023",
+        instituicao: "Alura",
+        sintese: "Estruturação de dados de maneira organizada, utilizando listas e coleções, permitindo criar programas mais robustos e flexíveis."
+    },
+]
+const contato = [
+    {
+        contato: "37 9 99685907"
+    },
+    {
+        contato: "gabrielbjbueno@gmail.com"
+    },
+    {
+        contato: "gabrielbueno@gmail.com"
+    }
+]
---
+++ js/portifolio.js
@@ -0,0 +1,82 @@
+const frm = document.querySelector("form");
+const conteudo = document.getElementById("conteudo");
+let estadoGuia
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(estadoGuia == 2){
+        apagaConteudo();
+    }
+    alteraGuia(estadoGuia = 1)
+    const dvBox = document.createElement("div");
+    dvBox.className = "pagina-conteudo-sobremim";
+    dvBox.id = "conteudo2";
+    conteudo.appendChild(dvBox);
+    sobreMim.forEach(textos => {
+        const texto = document.createTextNode(textos.paragrafo);
+        const p = document.createElement("p");
+        p.appendChild(texto);
+        dvBox.appendChild(p);
+    });
+    btSobreMim.disabled = true;
+});
+frm.btFormacao.addEventListener("click", () => {
+    if(estadoGuia == 1){
+        apagaConteudo();
+    }
+    alteraGuia(estadoGuia = 2)
+    const dvBox = document.createElement("section");
+    dvBox.id = "conteudo2"
+    conteudo.appendChild(dvBox)
+    dvBox.className = "pagina-conteudo-scroll"
+    formacaoCursos.forEach(cursos => {
+        const dvBoxes = document.createElement("div");
+        dvBox.appendChild(dvBoxes);
+        dvBoxes.className = "pagina-conteudo-formacao"
+        const cursoAno = document.createTextNode(`${cursos.nome} - ${cursos.ano}`);
+        const cursoInstiuicao = document.createTextNode(cursos.instituicao);
+        const cursoSintese = document.createTextNode(cursos.sintese)
+        const h4 = document.createElement("h4");
+        dvBoxes.appendChild(h4)
+        h4.appendChild(cursoAno);
+        const h5 = document.createElement("h5");
+        dvBoxes.appendChild(h5);
+        h5.appendChild(cursoInstiuicao);
+        const p = document.createElement("p");
+        dvBoxes.appendChild(p);
+        p.appendChild(cursoSintese)
+    });
+});
+
+function apagaConteudo () {
+    const boxConteudo = document.getElementById("conteudo2")
+    conteudo.removeChild(boxConteudo);
+};
+
+function alteraGuia (estado) {
+    if(estado == 1){
+        btSobreMim.className = "guia-selecionada";
+        btFormacao.className = "guia-desselecionada";
+        btContato.className = "guia-desselecionada";
+        btSobreMim.disabled = true;
+        btFormacao.disabled = false;
+        btContato.disabled = false;
+    }else if(estado == 2){
+        btSobreMim.className = "guia-desselecionada"
+        btFormacao.className = "guia-selecionada"
+        btContato.className = "guia-desselecionada"
+        btSobreMim.disabled = false;
+        btFormacao.disabled = true;
+        btContato.disabled = false;
+    }else{
+        btSobreMim.className = "guia-desselecionada"
+        btFormacao.className = "guia-desselecionada"
+        btContato.className = "guia-selecionada"
+        btSobreMim.disabled = false;
+        btFormacao.disabled = false;
+        btContato.disabled = true;
+    }
+}
+window.onload = function(){
+    document.getElementById("btSobreMim").click();
+};
